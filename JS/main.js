ac31a98518a1dfa0b3bde6550453a31e25dace6d
Akram-HANY <ibnhany595@gmail.com>
2025-03-25 02:05:43 +0200

    finish - Release

--- JS/main.js
+++ JS/main.js
@@ -1,21 +1,34 @@
-$(document).ready(function(){
+$(document).ready(function () {
 
-$('.fa-bars').click(function(){
-  $(this).toggleClass('fa-times'); 
-  $('.navbar').toggleClass('nav-toggle');
-});
+    $('.fa-bars').click(function () {
+        $(this).toggleClass('fa-times');
+        $('.navbar').toggleClass('nav-toggle');
+    });
 
-$(Window).on('scroll load',function(){
+    $(Window).on('scroll load', function () {
 
-    $('.fa-bars').removeClass('fa-times'); 
-    $('.navbar').removeClass('nav-toggle');
+        $('.fa-bars').removeClass('fa-times');
+        $('.navbar').removeClass('nav-toggle');
 
-    if($(Window).scrollTop() > 30){
-        $('header').addClass('header-active');
-    }else{
-        $('header').removeClass('header-active');
-    }
+        if ($(Window).scrollTop() > 30) {
+            $('header').addClass('header-active');
+        } else {
+            $('header').removeClass('header-active');
+        }
 
-});
- 
+
+
+        $('section').each(function () {
+            var id = $(this).attr('id');
+            var height = $(this).height();
+            var offset = $(this).offset().top - 200;
+            var top = $(Window).scrollTop();
+            if (top >= offset && top < height + offset) {
+                $('.navbar ul li a').removeClass('active');
+                $('.navbar').find('[href="#' + id + '"]').addClass('active');
+            }
+        });
+
+    });
+
 }); 
